067095555e84870c4961dcd379ebb4eadf306d2c
Ajdin <ajdin.kurtovic@edu.fit.ba>
2022-07-17 17:20:10 +0200

    initial commit

---
+++ EmployeeRegistration/Program.cs
@@ -0,0 +1,73 @@
+//using InspectionAPI.Data;
+//using Microsoft.EntityFrameworkCore;
+
+
+
+//var myAllowSpacificOrigins = "_myAllowSpacificOrigins";
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+//builder.Services.AddDbContext<DataContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
+////Enable CORS
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy(name: myAllowSpacificOrigins,
+//        builder =>
+//        {
+//            builder.WithOrigins("http://localhost:4200")
+//            .AllowAnyMethod()
+//            .AllowAnyHeader();
+//        });
+//});
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseCors(myAllowSpacificOrigins);
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InspectionAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
